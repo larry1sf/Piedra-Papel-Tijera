32025dff3551cddbe3dd6b2169057abf77b56e17
larry <larryycel@gmail.com>
2024-07-18 02:00:14 -0500

    sin imagenes ni toast ni laterales

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/* eslint-disable semi */
+const Toast = document.getElementById("toast")
+Toast.className = "d-none"
+
+const btnDerecha = document.getElementById("btn_derecha")
+const btnIzquierda = document.getElementById("btn_izquierda")
+
+const Pizquierda = document.getElementById("izquierda")
+const Pderecha = document.getElementById("derecha")
+
+const Pselecion = document.getElementById("seleccion")
+const Pcentral = document.getElementById("central")
+
+const crearJugadores = (p = []) => {
+  p.forEach((e) => { e.className = "col-3 d-flex justify-content-center align-items-center" })
+}
+
+function ajusteCentral() {
+  const classAsignar = "col-6 text-center mt-5 bg-img"
+  Pcentral.className = classAsignar
+  crearJugadores([Pderecha, Pizquierda])
+  Pselecion.className = "d-none"
+}
+
+const seleccionLado = (btn) => { btn.addEventListener("click", ajusteCentral) }
+
+seleccionLado(btnIzquierda)
+seleccionLado(btnDerecha)
