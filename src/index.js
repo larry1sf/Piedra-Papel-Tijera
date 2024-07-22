055c4199e5163325818925c95d06420cbd54c8fe
larry <larryycel@gmail.com>
2024-07-21 22:03:29 -0500

    arreglando el js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-/* eslint-disable no-return-assign */
 /* eslint-disable semi */
-// anulando el toast por el momento.
-// const Toast = document.getElementById("toast")
-// Toast.className = "d-none"
-
 const btnDerecha = document.getElementById("btn_derecha")
 const btnIzquierda = document.getElementById("btn_izquierda")
-
 const Pizquierda = document.getElementById("izquierda")
 const Pderecha = document.getElementById("derecha")
-
 const Pselecion = document.getElementById("seleccion")
 const Pcentral = document.getElementById("central")
 
 const crearJugadores = (p = []) => {
-  p.forEach(e => e.className = "col-3 d-flex justify-content-center align-items-center")
+  p.forEach((e) => { e.className = "col-3 d-flex justify-content-center align-items-center" })
 }
 
-function ajustePage(x = HTMLElement) {
+const ajustePage = (x = HTMLElement) => {
   const classAsignar = "col-6 text-center mt-5 bg-img"
   Pcentral.className = classAsignar
   Pselecion.className = "d-none";
